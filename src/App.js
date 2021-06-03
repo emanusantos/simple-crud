50c36e34d59f53d2bb9657d91439744fb352e676
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-03 13:02:32 -0300

    Added inputs and labels

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="inputContainer">
+        <label htmlFor="">Name</label>
+        <input type="text" />
+        <label htmlFor="">Age</label>
+        <input type="text" />
+        <label htmlFor="">Country</label>
+        <input type="text" />
+        <label htmlFor="">Position</label>
+        <input type="text" />
+        <label htmlFor="">Wage (year)</label>
+        <input type="text" />
+      </div>
     </div>
   );
 }
