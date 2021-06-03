3004e21dd92e72320efbf2a5058b835cfe2fc21d
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-03 17:29:32 -0300

    Added Axios and used its post methods for the front-end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
 
   const [name, setName] = useState('')
-  const [age, setAge] = useState('')
+  const [age, setAge] = useState(0)
   const [country, setCountry] = useState('')
   const [position, setPosition] = useState('')
-  const [wage, setWage] = useState('')
+  const [wage, setWage] = useState(0)
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name, 
+      age: age, 
+      country: country, 
+      position: position, 
+      wage: wage
+    }).then(() => {
+      console.log("Sucess");
+    })
+  }
 
   return (
     <div className="App">
@@ -22,7 +35,7 @@ function App() {
         <input type="text" onChange={(event) => {setPosition(event.target.value)}} />
         <label htmlFor="">Wage (year):</label>
         <input type="text" onChange={(event) => {setWage(event.target.value)}} />
-        <button>Add employee!</button>
+        <button onClick={addEmployee}>Add employee!</button>
       </div>
     </div>
   );
