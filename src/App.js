314afc249a90abbbe977d292735b156483d3b79b
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-03 13:11:07 -0300

    Stylized the index

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ function App() {
   return (
     <div className="App">
       <div className="inputContainer">
-        <label htmlFor="">Name</label>
+        <label htmlFor="">Name:</label>
         <input type="text" />
-        <label htmlFor="">Age</label>
+        <label htmlFor="">Age:</label>
         <input type="text" />
-        <label htmlFor="">Country</label>
+        <label htmlFor="">Country:</label>
         <input type="text" />
-        <label htmlFor="">Position</label>
+        <label htmlFor="">Position:</label>
         <input type="text" />
-        <label htmlFor="">Wage (year)</label>
+        <label htmlFor="">Wage (year):</label>
         <input type="text" />
+        <button>Add employee!</button>
       </div>
     </div>
   );
