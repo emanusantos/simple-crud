5aa918a7faf9242f59943ac4cce2838aaf8a40d8
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-03 13:16:42 -0300

    Added the input states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [country, setCountry] = useState('')
+  const [position, setPosition] = useState('')
+  const [wage, setWage] = useState('')
+
   return (
     <div className="App">
       <div className="inputContainer">
         <label htmlFor="">Name:</label>
-        <input type="text" />
+        <input type="text" onChange={(event) => {setName(event.target.value)}} />
         <label htmlFor="">Age:</label>
-        <input type="text" />
+        <input type="text" onChange={(event) => {setAge(event.target.value)}} />
         <label htmlFor="">Country:</label>
-        <input type="text" />
+        <input type="text" onChange={(event) => {setCountry(event.target.value)}} />
         <label htmlFor="">Position:</label>
-        <input type="text" />
+        <input type="text" onChange={(event) => {setPosition(event.target.value)}} />
         <label htmlFor="">Wage (year):</label>
-        <input type="text" />
+        <input type="text" onChange={(event) => {setWage(event.target.value)}} />
         <button>Add employee!</button>
       </div>
     </div>
